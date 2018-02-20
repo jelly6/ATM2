b2653684d0501204251cb6a5486bc9b1fbcffd16
Jelly <jelly13.chen@gmail.com>
2018-02-20 23:15:43 +0800

    ADD expanse.db Table(debug  .db)

--- app/src/main/java/com/scc/jellyfish6/atm/AddActivity.java
+++ app/src/main/java/com/scc/jellyfish6/atm/AddActivity.java
@@ -1,7 +1,9 @@
 package com.scc.jellyfish6.atm;
 
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,12 +14,16 @@ public class AddActivity extends AppCompatActivity {
     private EditText edDetail;
     private EditText edAmount;
     private Button b_add;
+    private MyDBHelper helper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         findViews();
+        helper = new MyDBHelper(this,"expanse.db",null,1);
+
 
     }
 
@@ -28,6 +34,21 @@ public class AddActivity extends AppCompatActivity {
         b_add = findViewById(R.id.b_add);
     }
     public void add(View v){
+        //findViews();
+        String cdate= edDate.getText().toString();
+        //Toast.makeText(this,cdate+"成功",Toast.LENGTH_LONG).show();
+        String details=edDetail.getText().toString();
+        //Toast.makeText(this,details+"成功",Toast.LENGTH_LONG).show();
+        int amount=Integer.parseInt(edAmount.getText().toString());
+        //Toast.makeText(this,amount+"成功",Toast.LENGTH_LONG).show();
+
+        ContentValues values=new ContentValues();
+        values.put("cdate", cdate);
+        values.put("details",details);
+        values.put("amount",amount);
+        long id=helper.getWritableDatabase().insert("exp",null,values);
+        //Toast.makeText(this,id+"ADD成功",Toast.LENGTH_LONG).show();
+        Log.d("ADD",id+"");
 
     }
 }
