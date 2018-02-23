ad5be0cb8ac474e42cc02d0d9b6442ed0f5698b3
jelly chen <jelly13.chen@itei.org.tw>
2018-02-23 14:51:01 +0800

    Git pull專案後Motify the files

--- app/src/main/java/com/scc/jellyfish6/atm/MainActivity.java
+++ app/src/main/java/com/scc/jellyfish6/atm/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main_xml,menu);
+        getMenuInflater().inflate(R.menu.menu_main,menu);
         return true;
     }
 
